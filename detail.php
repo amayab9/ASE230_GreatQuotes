778ba6f3ae3139b45371f8ea0a5716410b5aabe2
AmayaB <bryanta21@nku.edu>
2022-10-14 11:54:59 -0400

    basics

---
+++ detail.php
@@ -0,0 +1,15 @@
+<?php
+        $fh = fopen('authors.csv', 'r');
+        $line_counter=0;
+        while($line=fgets($fh)){
+          if($line_counter==$_GET['index']){
+            //display the author
+            echo $line;
+          }
+          $line_counter++;
+        }
+        fclose($fh);
+?>
+
+<a class="btn btn-secondary" href="modify.php?index=<?= $_GET['index'] ?>">modify author</a>
+      <a class="btn btn-danger" href="delete.php?index=<?= $_GET['index'] ?>">delete author</a>
---
+++ index.php
@@ -0,0 +1,16 @@
+<h2><a href="create.php">Add a new author</a><hr /></h2>
+<?php
+            $fh = fopen('authors.csv', 'r'); //open authors page in read mode
+            $index=0;
+            while($line=fgets($fh)){
+              if(strlen(trim($line))>0) {
+                echo '<h1><a href="detail.php?index='.$index.'">'.trim($line).'</a>
+                (<a href="detail.php?index='.$index.'">view author</a>)
+                (<a href="modify.php?index='.$index.'">modify author</a>)
+                (<a href="delete.php?index='.$index.'">delete author</a>)</h1>';
+                $index++;
+              }
+
+            } //read line by line
+            fclose($fh); //close file
+        ?>
