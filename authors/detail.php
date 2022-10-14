09cbc38bb1d647d3b478be58ccedf8abb8c99af3
AmayaB <bryanta21@nku.edu>
2022-10-14 13:37:47 -0400

    attempting to display quotes via sessions

--- authors/detail.php
+++ authors/detail.php
@@ -16,3 +16,19 @@ session_start();
 <a href='../quotes/create.php' >Add a quote</a>
 <a class="btn btn-secondary" href="modify.php?index=<?= $_GET['index'] ?>">modify author</a>
 <a class="btn btn-danger" href="delete.php?index=<?= $_GET['index'] ?>">delete author</a>
+<p> Quotes by Author</p>
+
+<?php
+  $fh = fopen('../data/quotes.csv', 'r');
+  $line_counter=0;
+  while($line=fgets($fh)){
+    if($line_counter==$_GET['index'] && $line_counter== $_SESSION['authorLineNumber']){//if index of author matches the index on this page
+      $line=trim($line);
+      $line=explode(';', $line);
+      if($line[1]==trim($_SESSION['authorLineNumber'])){
+        echo $line;
+      }
+    }
+    $line_counter++;
+  }
+  fclose($fh);
